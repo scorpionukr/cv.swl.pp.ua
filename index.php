8677669c5854aca29e8e41bc5fc15596342c744b
scorpionukr <scorpionukr@gmail.com>
2018-09-21 19:41:15 +0300

    first

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Bogachev Vitaliy CV</title>
+	<meta charset="utf-8">
+    <meta name="description" content="CV">
+    <meta name="keywords" content="CV">
+    <meta name="author" content="Scorpion">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+	<!-- JS -->
+	<script src="semantic/dist/semantic.min.js"></script>
+	<script>
+		$('.ui.sidebar').sidebar('toggle');
+	</script>
+</head>
+<body>
+	<div class="ui sidebar inverted vertical menu">
+		<a class="item">1</a>
+		<a class="item">2</a>
+		<a class="item">3</a>
+	</div>
+	<div class="pusher">
+		<!-- Site content !-->
+		<!-- Ciaeiinoai -->
+		<div class="back-welcome">
+		<div class="ui center aligned container">
+			<p>Bogachev Vitaliy CV</p>
+		</div>
+		</div>
+		<!-- Iiuo ?aaiou -->
+		<div class="ui raised very padded text container segment">
+			<h2 class="ui header">Dogs Roles with Humans</h2>
+			<p></p>
+			<p></p>
+		</div>
+		<div class="ui raised very padded text container segments">
+			<div class="ui segment">
+				<p>Top</p>
+			</div>
+			<div class="ui red segment">
+				<p>Middle</p>
+			</div>
+			<div class="ui blue segment">
+				<p>Middle</p>
+			</div>
+			<div class="ui green segment">
+				<p>Middle</p>
+			</div>
+			<div class="ui yellow segment">
+				<p>Bottom</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>

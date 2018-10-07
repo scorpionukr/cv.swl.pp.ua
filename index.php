580ccffe47f570023770f383ba3a84cfc7fcc82e
scorpionukr <scorpionukr@gmail.com>
2018-10-07 13:09:41 +0300

    minor changes

--- index.php
+++ index.php
@@ -65,6 +65,14 @@
 				<p>Bottom</p>
 			</div>
 		</div>
+		<div class="ui two column stackable grid">
+			<div class="column">
+				<div class="ui segment">Content</div>
+			</div>
+			<div class="column">
+				<div class="ui segment">Content</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
